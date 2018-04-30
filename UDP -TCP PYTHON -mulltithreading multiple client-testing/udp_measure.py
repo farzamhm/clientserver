2fad67f76e5749945e2b1364b0a014b5d342213e
farzamhm <32340254+farzamhm@users.noreply.github.com>
2018-04-30 19:24:24 -0400

    Add files via upload
    average delay and jitter calculation

--- UDP -TCP PYTHON -mulltithreading multiple client-testing/udp_measure.py
+++ UDP -TCP PYTHON -mulltithreading multiple client-testing/udp_measure.py
@@ -12,7 +12,7 @@ class UdpJitterStat:
             "\\" ,
             "/") + "/%s.csv"
         self.prev_seq = 0
-        self.info_config = str("UDP_IP , UDP_PORT" , )
+        self.info_config = str("UDP_IP , UDP_PORT, below info=jitter value + jitter average value" , )
         print(self.info_config , file=open(self.save_path_jitter % self.filename_jitter , 'a'))
         self.info_config = str([self.UDP_IP ,self.UDP_PORT])
         print(self.info_config , file=open(self.save_path_jitter % self.filename_jitter , 'a'))
@@ -20,6 +20,8 @@ class UdpJitterStat:
         self.D2 = 0
         self.recv_seq = 0
         self.loss=0
+        self.average_jitt=0
+        self.entry=0
 
 
     def run(self,data):
@@ -29,8 +31,9 @@ class UdpJitterStat:
             self.s_cr_time = float(datetime.now().strftime("%H:%M:%S.%f").split(':')[2])
             self.D2=self.s_cr_time-self.s_st_time
             self.recv_seq=int(data.decode().split("'")[1])
-            print(str(self.D2-self.D1), file=open(self.save_path_jitter % self.filename_jitter , 'a'))
-            # appendfile.write(str(D2 - D1) + '\n')
+            self.average_jitt = ((self.D2 - self.D1) + (self.average_jitt * self.entry)) / (self.entry + 1)
+            print(str(self.D2-self.D1)+","+str(self.average_jitt), file=open(self.save_path_jitter % self.filename_jitter , 'a'))
+            self.entry += 1
             self.D1=self.D2
             self.prev_seq=self.recv_seq
 
